e6c8545f68d8c18bcb947cbdb0454d05e5a99d26
Ivan Manchur <ivan.manchur47@gmail.com>
2025-08-12 13:22:39 +0300

    Template is changed to be Turtles-like

--- builder/content/contest/template.hpp
+++ builder/content/contest/template.hpp
@@ -1,28 +1,53 @@
-// hash = 9fd99b
+// hash = 44bf95
  
 #include <bits/stdc++.h>
 using namespace std;
 
 #define FOR(i, a, b) for(int i = (a); i < (b); i++)
 #define RFOR(i, a, b) for(int i = (a) - 1; i >= (b); i--)
-#define SZ(a) int(a.size())
-#define ALL(a) a.begin(), a.end()
-#define PB push_back
+#define sz(a) int(a.size())
+#define all(a) a.begin(), a.end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define pb push_back
 #define MP make_pair
-#define F first
-#define S second
+#define x first
+#define y second
 
-typedef long long LL;
+typedef long long ll;
+typedef double db;
+typedef long double LD;
+typedef pair<int, int> pii;
+typedef pair<db, db> pdd;
+typedef pair<ll, ll> pll;
 typedef vector<int> VI;
 typedef vector<LL> VL;
-typedef pair<int, int> PII;
-typedef pair<LL, LL> PLL;
-typedef double db;
 
-int main()
+int solve()
 {
-	ios::sync_with_stdio(0); 
-	cin.tie(0);
-	
-	return 0;
+  int n;
+  if (!(cin >> n))
+    return 1;
+  
+  return 0;
+}
+
+int32_t main()
+{
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  
+  int TET = 1e9;
+  //cin >> TET;
+  for (int i = 1; i <= TET; ++i)
+  {
+    if (solve())
+      break;
+    #ifdef ONPC
+      cerr << "________________________________\n";
+    #endif
+  }
+  #ifdef ONPC
+    cerr << "\nfinished in " << clock() * 1. / CLOCKS_PER_SEC << " sec\n";
+  #endif
+  return 0;
 }
